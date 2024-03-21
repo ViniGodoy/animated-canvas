933aa0d6e97659600b285389c278ad0e6d912727
Vinícius Mendonça <vinicius.mendonca@xa.epicgames.com>
2024-03-21 18:28:55 -0300

    Code implementation

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+'use strict'
+
+let y = 0;
+
 function process(time) {
-    angle += 0.1 * time;
+    y += 100 * time;
+    if (y > 420) y = -20;
 }
 
 function draw(ctx) {
+    ctx.clearRect(0, 0, 400, 400);
     ctx.beginPath();
     ctx.strokeStyle = "black";
-    ctx.fillStyle = "green";
+    ctx.fillStyle = "pink";
     ctx.lineWidth = 2;
 
-    ctx.arc(95, 50, 20, angle, 1 * Math.PI);
+    ctx.arc(200, y, 20, 0, 2 * Math.PI);
+
     ctx.fill();
     ctx.stroke();
 }
@@ -16,26 +23,27 @@ function draw(ctx) {
 
 
 
-let startTime = 0;
-
-function onFrame(time) {
-    if (startTime === 0) {
-        startTime = time;
-    } else {
-        const elapsed = (startTime - time) / 1000;
-        process(elapsed)
-    }
-    window.requestAnimationFrame(onFrame);
-}
-
+let lastFrameTime = 0;
 
 function beginAnimation() {
     const canvas = document.querySelector('#canvas');
     if (!canvas.getContext) {
         alert("Canvas não disponível!");
-    } else {
-        draw(canvas.getContext('2d'));
+        return;
+    }
+    function onFrame(time) {
+        if (lastFrameTime === 0) {
+            lastFrameTime = time;
+        } else {
+            const elapsed = (time - lastFrameTime) / 1000;
+            process(elapsed);
+            draw(canvas.getContext('2d'));
+            lastFrameTime = time;
+        }
+        window.requestAnimationFrame(onFrame);
     }
 
     window.requestAnimationFrame(onFrame);
 }
+
+beginAnimation();
