28e3193ef5bd23bf938608a9c425aff8ab9f774c
vinigodoy <vinigodoy@gmail.com>
2024-04-03 22:34:39 -0300

    Ajustes no código

--- animation.js
+++ animation.js
@@ -41,11 +41,9 @@ function beginAnimation() {
     let lastFrameTime = 0;
 
     function onFrame(time) {
-        if (lastFrameTime === 0) {
-            //Não temos como calcular a diferença de tempo entre dois quadros no primeiro quadros
-            //Por isso, pulamos ele sem desenhar
-            lastFrameTime = time;
-        } else {
+        //Não temos como calcular a diferença de tempo entre dois quadros no primeiro quadro
+        //Por isso, se for ele (lastFrameTime === 0) pulamos ele sem desenhar
+        if (lastFrameTime !== 0) {
             //Calculamos o tempo transcorrido, em segundos
             const elapsed = (time - lastFrameTime) / 1000;
             update(elapsed); //Atualizamos a lógica
@@ -54,13 +52,14 @@ function beginAnimation() {
             ctx.save();
             draw(ctx);
             ctx.restore();
-
-            //Guardamos o tempo desse quadro para usar no próximo desenho
-            lastFrameTime = time;
         }
+        //Guardamos o tempo desse quadro para usar no próximo desenho
+        lastFrameTime = time;
+        //Solicitamos o próximo quadro
         window.requestAnimationFrame(onFrame);
     }
 
+    //Solicita o primeiro quadro
     window.requestAnimationFrame(onFrame);
 }
 
